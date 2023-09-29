a2934cf82c2ab8bb074a9e6d841fbf4d87723b4f
Manu <manugeo13@gmail.com>
2023-09-29 16:04:26 +0530

    Blog detaisl - Added edit & delete buttons.

--- src/components/pages/blog-details.jsx
+++ src/components/pages/blog-details.jsx
@@ -4,10 +4,12 @@ import { useParams } from "next/navigation";
 import { TextH3, TextP } from "../ui/texts";
 import useBlogsFromContext from "@/hooks/useBlogsFromContext";
 import Separator from "../ui/separator";
+import { Button } from "../ui/button";
+import { ChevronLeft, Pencil, Trash2 } from "lucide-react";
 
 const BlogDetails = () => {
   const params = useParams()
-  const { blogs } = useBlogsFromContext();
+  const { blogs, isInitializing } = useBlogsFromContext();
   const currentBlog = blogs.find(blog => blog.id === params.id);
 
   console.log('params:', params, 'blogs :', blogs);
@@ -15,9 +17,23 @@ const BlogDetails = () => {
 
   return (
     <div className="flex flex-col pb-6">
-      <TextH3 className="mt-8 px-6">{currentBlog.title}</TextH3>
-      <Separator className="mt-6 mx-6" />
-      <TextP className="mt-6 px-6 whitespace-pre-wrap">{currentBlog.description}</TextP>
+      <div className="mt-4 mx-6 flex">
+        <Button variant="outline" size="icon"><ChevronLeft /></Button>
+        {currentBlog && <>
+          <Button variant="outline" size="icon" className="ml-auto"><Pencil /></Button>
+          <Button variant="outline" size="icon" className="ml-1"><Trash2 /></Button>
+        </>}
+      </div>
+      {isInitializing ? (
+        <div className={'mt-4 px-6'}>
+          <TextP className="text-center">Loading Blogs, Please Wait...</TextP>
+        </div>
+      ) : (currentBlog && <>
+        <TextH3 className="mt-4 px-6">{currentBlog.title}</TextH3>
+        <Separator className="mt-6 mx-6" />
+        <TextP className="mt-6 px-6 whitespace-pre-wrap">{currentBlog.description}</TextP>
+      </>)}
+
     </div>
   );
 };
